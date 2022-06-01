63351e1d1ee94d6e8eb446b84ddb0e4dd864b432
Patrick Flügge <patrick.fluegge@mailbox.org>
2022-06-01 17:07:04 +0200

    feat: added doc blocks for the Schedule Model

--- src/Eloquent/Schedule.php
+++ src/Eloquent/Schedule.php
@@ -5,7 +5,35 @@ namespace Schruptor\Schedule\Eloquent;
 use Carbon\Carbon;
 use Cron\CronExpression;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Schruptor\Schedule\Database\Factories\ScheduleFactory;
+
+/**
+ * App\Models\Ping
+ *
+ * @property int $id
+ * @property int $job_id
+ * @property string|null $ip
+ * @property string|null $expression
+ * @property string|null $user
+ * @property carbon|null $next_run_date
+ * @property string|null $command
+ * @property string|null $description
+ * @property bool|null $evenInMaintenanceMode
+ * @property bool|null $withoutOverlapping
+ * @method static ScheduleFactory factory(...$parameters)
+ * @method static Builder|Schedule newModelQuery()
+ * @method static Builder|Schedule newQuery()
+ * @method static Builder|Schedule query()
+ * @method static Builder|Schedule whereCount($value)
+ * @method static Builder|Schedule whereCreatedAt($value)
+ * @method static Builder|Schedule whereId($value)
+ * @method static Builder|Schedule whereIp($value)
+ * @method static Builder|Schedule whereJobId($value)
+ * @method static Builder|Schedule whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 
 class Schedule extends Model
 {
@@ -21,7 +49,7 @@ class Schedule extends Model
         'withoutOverlapping',
     ];
 
-    public function nextRunDate()
+    public function nextRunDate(): Carbon
     {
         return new Carbon((new CronExpression($this->expression))->getNextRunDate(
             currentTime: now(),
